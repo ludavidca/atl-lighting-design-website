10f44830ff0e5dd21c08a547d1da8414fe73f5e5
ludavidca <davidjylu7@gmail.com>
2024-09-06 14:05:03 -0600

    First Commit

---
+++ src/router/index.ts
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'About',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/projects',
+      name: 'Projects',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProjectsView.vue')
+    },
+    {
+      path: '/awards',
+      name: 'Awardss',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AwardsView.vue')
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ContactView.vue')
+    }
+  ]
+})
+
+export default router

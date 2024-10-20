6b7d0c5fef063229dea5b14fb62d9447752f13ca
ludavidca <davidjylu7@gmail.com>
2024-10-20 15:35:21 -0400

    made small change

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ const isActive = (path: string) => computed(() => route.path === path)
       <header class="no-scrollbar">
         <nav class="flex flex-row w-full absolute z-20 bg-gradient-to-b from-black/50 via-[rgba(28,28,28,0.20)] to-[rgba(37,37,37,0.00)]">
           <div class="w-1/2 pl-20 py-5">
-            <RouterLink to="/" ><img alt="Vue logo" src="@/assets/logo.svg"/></RouterLink>
+            <RouterLink to="/" ><img alt="Vue logo" src="/logo.svg"/></RouterLink>
           </div>
           <div class="w-1/2 flex items-center gap-x-20 justify-end pr-20">
             <RouterLink to="/projects" class="nav-link" :class="{ 'active': isActive('/projects').value }">Projects</RouterLink>
